64683614ed2068d7e5cc37a009e71fdecd55880a
ali27-dev <khanali3229@gmail.com>
2025-06-27 23:38:57 +0500

    i put responsiveness in protfolio.the protfolio will open in all type of mobiles and screen sizes,additionaly implement smooth scrolling,sticky navigation and lastly i put year and date change funtionality

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 /***********************/
 // mobile-nav
-let btnMobileNav = document.getElementById("btn-mobile-nav");
+const btnMobileNav = document.getElementById("btn-mobile-nav");
 const headerEl = document.getElementById("header");
 console.log(btnMobileNav);
 console.log(headerEl);
@@ -8,3 +8,63 @@ console.log(headerEl);
 btnMobileNav.addEventListener("click", function () {
   headerEl.classList.toggle("nav-open");
 });
+// ************************* //
+// smooth-scrolling
+
+const allLinks = document.querySelectorAll("a:link");
+
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+
+    // scroll back to top
+    if (href === "#")
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      console.log(sectionEl);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+    //mobile nav
+
+    if (link.classList.contains("nav-li-link"))
+      headerEl.classList.toggle("nav-open");
+  });
+});
+
+//*********************************/ /
+// sticky-nav
+const sectionheaderEl = document.querySelector(".hero-section");
+
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    console.log(ent);
+
+    if (ent.isIntersecting === false) {
+      document.body.classList.add("sticky");
+    }
+    if (ent.isIntersecting === true) {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    // in the viewport
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+obs.observe(sectionheaderEl);
+/*************************/
+// span current date
+const yearEl = document.querySelector(".year");
+
+const newYear = new Date().getFullYear();
+console.log(newYear);
+yearEl.textContent = newYear;
